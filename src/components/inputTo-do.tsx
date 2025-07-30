b6d82389b87343580ebdb4ed7a1b43a5ffccf731
vinicius campos <marco.marks06@gmail.com>
2025-07-30 06:22:35 -0300

    feat: adicionar e remover task do array guardando no cache

--- src/components/inputTo-do.tsx
+++ src/components/inputTo-do.tsx
@@ -1,5 +1,5 @@
 import calendar from "../assets/calendar.svg"
-
+import plusSvg from "../assets/plus.svg"
 /*
     Value do Input: é passado pelo pai, mas é preciso passar como uma propriedade (Props)
     onAddTask: é o evento de butão, estamos usando aqui o React controlado
@@ -12,27 +12,37 @@ type Props = React.ComponentProps<"input"> & {
 export function InputComponets({ onAddTask, ...rest }: Props) {
     return (
         <div className="bg-[#18181B] flex items-center justify-center border-2 border-[#27272A] my-12 mx-4 rounded-2xl">
-            <fieldset className="flex justify-between p-4">
+            <fieldset className="flex justify-between p-4 w-full items-center">
 
                 <div className="flex gap-4 items-center">
 
                     <span>
-                        <img src={calendar} alt="calendar" className="" />
+                        <img src={calendar} alt="calendar" className="w-6 h-6" />
                     </span>
 
                     <input
                         type="text"
                         placeholder="Escreva uma task"
-                        className="text-md text-[#A1A1AA] border-none leading-6 focus:outline-none flex-grow"
+                        className="text-md text-[#A1A1AA] border-none leading-6 focus:outline-none flex-grow w-40"
                         {...rest}
                     />
                 </div>
+                    <span className="text-zinc-100/40">|</span>
 
                 <button
-                    className="flex items-center justify-center gap-2 bg-lime-400 rounded-sm  px-2 py-1 cursor-pointer"
+                    className="flex items-center justify-center gap-2 bg-lime-400 hover:bg-lime-800 rounded-sm  px-2 py-1 cursor-pointer"
                     onClick={onAddTask}
                 >
-                    <span className="">Adicionar</span>
+                    <div className="flex items-center gap-2">
+                        <span className="">Adicionar</span>
+                        <span>
+                            <img
+                                src={plusSvg}
+                                alt="plus"
+                                className="w-4 h-4"
+                            />
+                        </span>
+                    </div>
                 </button>
             </fieldset>
         </div>
--- src/hooks/todo.tsx
+++ src/hooks/todo.tsx
@@ -1,14 +1,39 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { InputComponets } from "../components/inputTo-do";
 import circleDefault from "../assets/circle-dashed.svg";
 import deleteSvg from "../assets/delete.svg"
+import checkSvg from "../assets/circle-check.svg";
 
-/**
- * Deve criar um estado para armazenar o valor do input
- */
+interface Task {
+    id: string;
+    text: string;
+    isCompleted: boolean;
+}
 
 export default function Todo() {
     const [taskText, setTaskText] = useState('');
+
+    const [tasks, setTasks] = useState<Task[]>([]);
+
+    // Carrega tarefas ao montar o componente
+    useEffect(() => {
+        if (typeof chrome !== 'undefined' && chrome.storage && chrome.storage.local) {
+            chrome.storage.local.get('tasks', (result) => {
+                if (result.tasks) {
+                    setTasks(result.tasks);
+                }
+            });
+        }
+    }, []) // efeito executado apenas uma vez quando o componente é montado
+
+    // Função para salvar tarefas no ARMAZENAMENTO LOCAL
+    const saveTasksToLocalStorage = (updateTasks: Task[]) => {
+        if (typeof chrome !== 'undefined' && chrome.storage && chrome.storage.local) {
+            chrome.storage.local.set({ tasks: updateTasks }, () => {
+                console.log('Tarefas salvas no armazenamento local.');
+            });
+        }
+    }
 
     const handlueInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         setTaskText(event.target.value);
@@ -17,17 +42,40 @@ export default function Todo() {
     const handleAddTesk = () => {
         // verificação se o botao esta vazio
         if (taskText.trim() !== '') {
-            console.log("Valor da task a ser adicionada", taskText)
+            const newTask: Task = {
+                id: Date.now().toString(),
+                text: taskText,
+                isCompleted: false, // Define se a task esta completa ou não
+            }
 
-            alert(`Task ${taskText} adicionada!`)
-
-            // !!! AQUI PODERIA ENVIAR A TASK PARA UMA API
-
-            setTaskText('')
+            setTasks((prevTasks) => {
+                const updatedTasks = [...prevTasks, newTask];
+                saveTasksToLocalStorage(updatedTasks);
+                return updatedTasks;
+            });
+            setTaskText('');
         } else {
             alert("Por favor! Digite a task antes de adicionar")
         }
     }
+
+    const handleToggleComplete = (id: string) => {
+        setTasks(prevTasks => {
+            const updatedTasks = prevTasks.map(task =>
+                task.id === id ? { ...task, isCompleted: !task.isCompleted } : task
+            );
+            saveTasksToLocalStorage(updatedTasks);
+            return updatedTasks;
+        })
+    }
+
+    const handleDeleteTask = (id: string) => {
+        setTasks(prevTasks => {
+            const updatedTasks = prevTasks.filter(task => task.id !== id);
+            saveTasksToLocalStorage(updatedTasks);
+            return updatedTasks
+        });
+    };
 
     return (
         <div className="flex flex-col gap-2">
@@ -37,21 +85,39 @@ export default function Todo() {
                 onAddTask={handleAddTesk} />
 
             <div>
-                <div className="flex items-center justify-between mx-5 
-                bg-zinc-900 border-[#27272A] border-[1px] p-2 px-4 rounded-xl">
-                    <div className="flex gap-2 items-center">
-                        <span className="cursor-pointer">
-                            <img src={circleDefault} alt="circle" />
-                        </span>
+                {tasks.length === 0 ? (
+                    <p className="text-center text-gray-500">Nenhuma tarefa adicionada</p>
+                ) : (
+                    tasks.map((task) => (
+                        <div
+                            key={task.id}
+                            className="flex items-center justify-between mx-5
+                            bg-zinc-900 border-[#27272A] border-[1px] p-2 px-4 rounded-xl mb-2"
+                        >
+                            <div className="flex gap-2 items-center">
+                                <span className="cursor-pointer" onClick={() => handleToggleComplete(task.id)}>
+                                    {task.isCompleted ? (
+                                        <img src={checkSvg} alt="círculo marcado" className="w-7 h-7" />
+                                    ) : (
+                                        <img src={circleDefault} alt="círculo padrão" />
+                                    )}
+                                </span>
 
-                        <p className="text-[#A1A1AA]">Terminar projeto</p>
-                    </div>
+                                <p className={`text-[#A1A1AA] ${task.isCompleted ? 'line-through text-gray-500' : ''}`}>
+                                    {task.text}
+                                </p>
+                            </div>
 
-                    <button className="cursor-pointer p-2 bg-transparent hover:bg-[#212126] transition ease-linear duration-400 rounded-[8px]">
-                        <img src={deleteSvg} alt="icon of delete" />
-                    </button>
-                </div>
+                            <button
+                                className="cursor-pointer p-2 bg-transparent hover:bg-[#212126] transition ease-linear duration-200 rounded-[8px]"
+                                onClick={() => handleDeleteTask(task.id)}
+                            >
+                                <img src={deleteSvg} alt="ícone de lixeira" />
+                            </button>
+                        </div>
+                    ))
+                )}
             </div>
         </div>
-    )
+    );
 }
